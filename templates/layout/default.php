646bc0e87c912583b1633cf4da06790a47e5a439
volchan0820 <volchan.se@gmail.com>
2025-12-04 09:10:59 +0900

    20251204

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -32,6 +32,15 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXX"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-S1RBCW3KD');
+    </script>
 </head>
 <body>
 <!--    
